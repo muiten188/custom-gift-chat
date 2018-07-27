5279c618b55ff595134f0880e0ceb7243d1f2592
ISPCO\bachbd <buidinhbach123@gmail.com>
2018-07-27 10:50:00 +0700

    #custom 1 for chat_app

---
+++ src/Color.js
@@ -0,0 +1,16 @@
+export default {
+  defaultColor: '#b2b2b2',
+  backgroundTransparent: 'transparent',
+  defaultBlue: '#f0f0f0',
+  leftBubbleBackground: '#448fcd',//'#0084ff',
+  white: '#fff',
+  carrot: '#e67e22',
+  emerald: '#2ecc71',
+  peterRiver: '#3498db',
+  wisteria: '#8e44ad',
+  alizarin: '#e74c3c',
+  turquoise: '#1abc9c',
+  midnightBlue: '#2c3e50',
+  optionTintColor: '#007AFF',
+  timeTextColor: '#aaa',
+};
